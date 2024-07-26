1808af259b4c0b5c6ba04cf7a7e9d393acff8ba4
Manasvisharma09 <22uec077@lnmiit.ac.in>
2024-07-26 09:30:57 +0530

    hey

--- appleclone/src/main.jsx
+++ appleclone/src/main.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as Sentry from "@sentry/react";
 
+Sentry.init({
+  dsn: "https://18a8244dd20baa627e6e84ac29e52cc1@o4507655349796864.ingest.us.sentry.io/4507656779923456",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
 
+  ],
+  
+  tracesSampleRate: 1.0, 
+  
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
